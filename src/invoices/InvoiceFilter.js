530bac0ad55cbdf6c2657ef40877d2128835fd7d
jankavka <jankavka@seznam.cz>
2025-02-04 19:25:59 +0100

    Filtration added

---
+++ src/invoices/InvoiceFilter.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import InputSelect from "../components/InputSelect";
+import InputField from "../components/InputField";
+
+
+const InvoiceFilter = (props) =>{
+
+
+    const handleChange = (e) =>{
+        props.handleChange(e);
+    }
+
+    const handleSubmit = (e) => {
+        props.handleSubmit(e);
+    }
+
+
+
+    const filter = props.filter;
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="row">
+                <div className="col">
+                    <InputSelect
+                        name="buyerId"
+                        handleChange={handleChange}
+                        value={filter.buyerId}
+                        items={props.persons}
+                        label="Kupující"
+                        prompt="nevybrán"
+                    />
+                </div>
+                <div className="col">
+                    <InputSelect
+                    name="sellerId"
+                    handleChange={handleChange}
+                    value={filter.sellerId}
+                    items={props.persons}
+                    label="Prodejce"
+                    prompt="nevybrán"
+                    />
+                </div>
+                <div className="col">
+                    <InputField
+                        name="product"
+                        handleChange={handleChange}
+                        value={filter.product}
+                        type="text"
+                        label="Produkt"
+                        prompt="Napište jméno produktu"
+                    />
+                </div>
+                <div>
+                    <InputField
+                        name="minPrice"
+                        handleChange={handleChange}
+                        value={filter.minPrice}
+                        type="number"
+                        label="Cena od"
+                        prompt=""
+                    />
+                </div>
+                <div>
+                    <InputField
+                        name="maxPrice"
+                        handleChange={handleChange}
+                        value={filter.maxPrice}
+                        type="number"
+                        label="Cena do"
+                        prompt=""
+                    />
+                </div>
+                <div>
+                    <InputField
+                        name="limit"
+                        handleChange={handleChange}
+                        value={filter.limit}
+                        type="number"
+                        label="limit"
+                    
+                    />
+                </div>
+
+                <div className="row">
+                <div className="col">
+                    <input
+                        type="submit"
+                        className="btn btn-secondary float-right mt-2"
+                        value={props.confirm}
+                    />
+                </div>
+            </div>
+
+            </div>
+        </form>
+    )
+}
+
+export default InvoiceFilter;
--- src/invoices/InvoiceIndex.js
+++ src/invoices/InvoiceIndex.js
@@ -3,9 +3,19 @@ import { useEffect, useState } from "react";
 import { apiDelete, apiGet } from "../utils/api";
 import InvoiceTable from "./InvoiceTable";
 import InvoiceStatistics from "./InvoiceStatistics";
+import InvoiceFilter from "./InvoiceFilter";
 
 const InvoiceIndex = () => {
     const [invoices, setInvoices] = useState([]);
+    const [personsState, setPersonsState] = useState([]);
+    const [filterState, setFilterState] = useState({
+        buyerId: undefined,
+        sellerId: undefined,
+        product: undefined,
+        minPrice: undefined,
+        maxPrice:undefined,
+        limit: undefined,
+    });
 
     const deleteInvoice = async (id) =>  {
         try{
@@ -20,11 +30,40 @@ const InvoiceIndex = () => {
 
     useEffect(() => {
         apiGet("/api/invoices").then((data) => setInvoices(data));
+        apiGet("/api/persons").then((data) => setPersonsState(data));
     }, [])
+
+    const handleChange = (e) => {
+        if (e.target.value === "false" || e.target.value === "true" || e.target.value === ""){
+            setFilterState(prevState => {
+                return{...prevState, [e.target.name]: undefined}
+            });
+        }else{
+            setFilterState((prevState) => {
+                return{...prevState, [e.target.name]:e.target.value}
+            });
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const params = filterState;
+        apiGet("/api/invoices", params).then((data) => setInvoices(data));
+
+    }
 
     return(
         <div>
             <h1>Seznam faktur</h1>
+            <hr/>
+            <InvoiceFilter
+                filter={filterState}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                persons={personsState}
+                confirm="Vyhledej"
+
+            />
             <InvoiceTable
                 items = {invoices}
                 deleteInvoice={deleteInvoice}
@@ -32,7 +71,7 @@ const InvoiceIndex = () => {
             />
             <InvoiceStatistics/>
         </div>
-    )
+    );
 }
 
 export default InvoiceIndex;
